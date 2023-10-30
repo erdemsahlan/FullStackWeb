88cea4e6b059fd05d168039543fde03c27915e33
Erdem Şahlan <sahlanerdem@hotmail.com>
2023-10-30 22:37:49 +0300

    SetDb

---
+++ Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Web.Models;
+
+namespace Web.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options) 
+        {
+            
+        }
+        
+
+        DbSet<Category> Categories { get; set; }
+    }
+}
